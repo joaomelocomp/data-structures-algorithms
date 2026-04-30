01ba473afcf56c73f265cd3e1a47835bf8a6bd50
João Gabriel Melo <joaogabriel.s.melo92@gmail.com>
2026-04-30 16:22:51 -0300

    Simplify minOperations method implementation
    Refactor minOperations to directly return the remainder of the sum divided by k instead of calculating operations.

--- leetcode/3512. Minimum Operations to Make Array Sum Divisible by K.py
+++ leetcode/3512. Minimum Operations to Make Array Sum Divisible by K.py
@@ -1,9 +1,3 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        total, count, index = sum(nums), 0, len(nums) - 1
-    
-        while total % k != 0:
-            total -= (nums[index] - 1)
-            count += 1
-        
-        return count
+        return sum(nums) % k
